b6d3e16c310b424fb888da822cff443a84983ef4
Marti Forn <magiclovett@gmail.com>
2026-07-19 16:13:06 +0200

    fix(nvim): show hidden and gitignored files in telescope find_files
    Add hidden=true and no_ignore=true to the <leader>ff mapping so that
    files inside ~/.config and other hidden or gitignored paths are visible.

--- .config/nvim/lua/magictt/plugins/telescope.lua
+++ .config/nvim/lua/magictt/plugins/telescope.lua
@@ -37,7 +37,9 @@ return {
     local keymap = vim.keymap -- for conciseness
 
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>ff', function()
+      builtin.find_files({ hidden = true, no_ignore = true })
+    end, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
