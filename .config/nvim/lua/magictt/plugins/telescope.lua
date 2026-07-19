f8f150db6c1260f9c130e73bbd8a91cdb412a7ae
Marti Forn <magiclovett@gmail.com>
2026-07-19 17:01:24 +0200

    fix(nvim): respect gitignore in telescope find_files

--- .config/nvim/lua/magictt/plugins/telescope.lua
+++ .config/nvim/lua/magictt/plugins/telescope.lua
@@ -38,7 +38,7 @@ return {
 
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
     vim.keymap.set('n', '<leader>ff', function()
-      builtin.find_files({ hidden = true, no_ignore = true })
+      builtin.find_files({ hidden = true })
     end, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
