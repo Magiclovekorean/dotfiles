34126c19bb58b5aab97aecc7aac59a9d0bc5351e
Marti Forn <magiclovett@gmail.com>
2026-08-09 22:46:05 +0200

    fix(hypr): drop xhost root access grant from autostart

--- .config/hypr/modules/autostart.lua
+++ .config/hypr/modules/autostart.lua
@@ -11,7 +11,6 @@ hl.on("hyprland.start", function ()
    hl.exec_cmd("waybar")
    hl.exec_cmd("hypridle")
    hl.exec_cmd("hyprpaper")
-   hl.exec_cmd("xhost +SI:localuser:root")
    hl.exec_cmd("wl-paste --type text --watch cliphist store")
    hl.exec_cmd("wl-paste --type image --watch cliphist store")
    -- clear clipboard history
