60b543ad0367b2d66003e957c3b3c4d9dc36333a
Marti Forn <magiclovett@gmail.com>
2026-08-14 00:32:47 +0200

    feat(nvim/core): set local leader to space

--- .config/nvim/lua/magictt/core/keymaps.lua
+++ .config/nvim/lua/magictt/core/keymaps.lua
@@ -1,5 +1,6 @@
 -- set leader key to space
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 local keymap = vim.keymap -- for conciseness
 
@@ -29,3 +30,4 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
