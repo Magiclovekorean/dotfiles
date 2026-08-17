7d7f73c5b9daff048cf29ed81db50c05373b2493
Marti Forn <magiclovett@gmail.com>
2026-08-18 00:14:41 +0200

    feat(nvim/telescope): map <leader>ff to git files

--- .config/nvim/lua/magictt/plugins/telescope.lua
+++ .config/nvim/lua/magictt/plugins/telescope.lua
@@ -37,9 +37,7 @@ return {
     local keymap = vim.keymap -- for conciseness
 
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-    vim.keymap.set('n', '<leader>ff', function()
-      builtin.find_files({ hidden = true })
-    end, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>ff', builtin.git_files, { desc = 'Telescope find git files' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
