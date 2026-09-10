2d8d93e8bde9d7fb76b46c9c174b86a4330d7207
Martí Forn <magiclovekorean@gmail.com>
2026-09-10 19:53:58 +0200

    chore(install): track hardware-configuration.nix in git

--- autoSetup.sh
+++ autoSetup.sh
@@ -4,19 +4,22 @@
 nixos-generate-config --root /mnt
 cp /mnt/etc/nixos/hardware-configuration.nix .
 
+# Give necessary permissions to magictt user
+chown -R magictt:users .
+chown -R magictt:users ..
+
 # Enable zram for compiling things like waybar
 modprobe zram
 zram_path=$(zramctl --find --size 4G)
 mkswap "$zram_path"
 swapon -p 100 "$zram_path"
 
-# hardware-configuration.nix is machine-specific and intentionally
-# excluded from the Git repository.
-echo "hardware-configuration.nix" >> .git/info/exclude
+git add hardware-configuration.nix
 
 # Install NixOS with flake and create password for magictt user, 
 # nixos-install will ask for the root password
 nixos-install --flake .#nixos-flake
+echo 'Setting password for magictt user'
 nixos-enter --root /mnt -c 'passwd magictt'
 
 read -p "Do you want to reboot? (Y/n) " answer
