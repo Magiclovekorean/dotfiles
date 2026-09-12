756380319cc4102bce0a3e127c55b0a2bea3275b
Martí Forn <magiclovekorean@gmail.com>
2026-09-12 18:15:31 +0200

    feat(install): add per-host setup flow with example template

--- autoSetup.sh
+++ autoSetup.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
+# Ask for hostname and save it to ~/.hostname
+read -p "Enter hostname: " hostname
+echo $(hostname) > /home/magictt/.hostname
+
+# Add device to hosts/
+cp hosts/example hosts/$(hostname)
+sed -i 's/<HOSTNAME>/$(hostname)/g' hosts/$(hostname)/*
+
 # Generate hardware-configuration.nix and move it to dotfiles dir
 nixos-generate-config --root /mnt
-cp /mnt/etc/nixos/hardware-configuration.nix .
+cp /mnt/etc/nixos/hardware-configuration.nix hosts/$(hostname)/hardware-configuration
 
 # Enable zram for compiling things like waybar
 modprobe zram
 zram_path=$(zramctl --find --size 4G)
 mkswap "$zram_path"
 swapon -p 100 "$zram_path"
-
-git add hardware-configuration.nix
 
 # Install NixOS with flake and create password for magictt user, 
 # nixos-install will ask for the root password
