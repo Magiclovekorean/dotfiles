39a6c653ec0397800d54626848726174d7d7b0ce
Martí Forn <magiclovekorean@gmail.com>
2026-09-23 22:20:36 +0200

    chore(hypr): pin browsers and workspaces to fixed monitors

--- home/config/hypr/modules/window_rules.lua
+++ home/config/hypr/modules/window_rules.lua
@@ -37,10 +37,22 @@ hl.window_rule({
 })
 
 hl.window_rule({
-    name = "brave-origin-browser-on-w1",
-    match = { class = "^(brave-origin)$" },
+    name = "librewolf-browser-on-w1",
+    match = { class = "^(librewolf)$" },
     workspace = "1",
 })
 
-hl.workspace_rule({ workspace = "10", monitor = "eDP-1" })
-hl.workspace_rule({ workspace = "1", monitor = "HDMI-A-1" })
+hl.window_rule({
+    name = "chromium-browser-on-w1",
+    match = { class = "^(chromium-browser)$" },
+    workspace = "1",
+})
+
+-- Assign workspaces 1 to 6 to HDMI-A-1 monitor and remaining to eDP-1
+for i = 1, 6, 1 do
+    hl.workspace_rule({ workspace = tostring(i), monitor = "HDMI-A-1" })
+end
+
+for i = 7, 10, 1 do
+    hl.workspace_rule({ workspace = tostring(i), monitor = "eDP-1" })
+end
